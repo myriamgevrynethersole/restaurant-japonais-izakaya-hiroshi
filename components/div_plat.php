7da7c90635d181cf827d2faa7e0925336f1e7d09
Myriam <myriamgevrynethersole@gmail.com>
2026-08-06 15:46:39 -0400

    add - ajout du contenue des plats principaux

--- components/div_plat.php
+++ components/div_plat.php
@@ -1,6 +1,6 @@
 <div class="swiper-slide">
     <div class="div--plat">
-        <img src="<?php echo $plat['images'][0] ?>" alt="" class="image--plat">
+        <img src="<?php echo $plat['images'] ?>" alt="" class="image--plat">
         <div class="info--plat">
             <h3><?php echo $plat['nom'] ?></h3>
             <p class="description"> <?php echo $plat['description']; ?> </p>
